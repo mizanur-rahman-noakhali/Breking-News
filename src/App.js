c829db276f1b9b3896ee4a5d1874898b2efd0190
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-10 19:46:55 +0600

    headline.length

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import {Button} from 'react-bootstrap';
 import News from './components/News/News';
+import Header from './components/Header/Header';
+import TopHeadline from './components/TopHeadline/TopHeadline';
 
 function App() {
   return (
     <div >
      <h1>Bootstraps</h1>
-     <Button variant="primary">Primary</Button>
+     <Header></Header>
+    <TopHeadline></TopHeadline>
      
-   <News></News>
+   
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+const Header = () => {
+  return (
+<div>
+    <h1>Breaking-news</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita dolores ipsum itaque pariatur, praesentium quibusdam, optio soluta odio repudiandae commodi natus. Consequuntur in culpa dolores nihil laboriosam iusto, harum praesentium.
+    </p>
+    <p>
+    <Button variant="primary">Primary</Button>
+    </p>
+    
+    </div>
+  
+  );
+};
+
+export default Header;
